2086a053dd74e5e4f4cbb89a55e28982ef1a40db
Kohei040 <i_kohei0316@icloud.com>
2019-06-28 19:14:59 +0900

    Update

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  watch: true,
   entry: { app: "./src/index.js" },
   output: {
-    path: __dirname + '/public',
-    filename: "bundle.js"
+    path: __dirname + '/public/js',
+    filename: "[name].js"
   },
   devServer: {
     contentBase: __dirname + '/public',
     port: 3000,
-    publicPath: 'bundle.js',
+    publicPath: '/js/',
     disableHostCheck: true,
     watchContentBase: true,
     watchOptions: {
-            poll: true
+            poll: 1000
     },
     hot: true,
     host: '0.0.0.0'
